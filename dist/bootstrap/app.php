5f9f3ee7ed280161c0267e46afb8b5dd3446904c
zidaibudai <coolchen.hao@gmail.com>
2017-06-06 20:33:25 +0800

    fix namespance bug.

--- dist/bootstrap/app.php
+++ dist/bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    Wechat\Http\Kernel::class
+    iBrand\WechatPlatform\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    Wechat\Console\Kernel::class
+    iBrand\WechatPlatform\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    Wechat\Exceptions\Handler::class
+    iBrand\WechatPlatform\Exceptions\Handler::class
 );
 
 /*
